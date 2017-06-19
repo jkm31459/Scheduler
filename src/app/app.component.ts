8f12460d1f692cd17adc38704686d1613c88081a
jkm31459 <jkmin314@gmail.com>
2017-06-18 21:53:24 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'app';
+  timeSlots: any[] = [
+    {
+      "name": '',
+      "phoneNumber": '',
+      "time": '9 AM'
+    },
+    {
+      "name": '',
+      "phoneNumber": '',
+      "time": '10 AM'
+    },
+    {
+      "name": '',
+      "phoneNumber": '',
+      "time": '11 AM'
+    },
+    {
+      "name": '',
+      "phoneNumber": '',
+      "time": '12 AM'
+    },
+    {
+      "name": '',
+      "phoneNumber": '',
+      "time": '1 PM'
+    },
+    {
+      "name": '',
+      "phoneNumber": '',
+      "time": '2 PM'
+    },
+    {
+      "name": '',
+      "phoneNumber": '',
+      "time": '3 PM'
+    },
+    {
+      "name": '',
+      "phoneNumber": '',
+      "time": '4 PM'
+    },
+    {
+      "name": '',
+      "phoneNumber": '',
+      "time": '5 PM'
+    }
+  ];
 }
